42c8564df4080874a9725e379adf767132b11bb8
Jake Mok Nelson <jakesmnelson@gmail.com>
2019-07-08 22:18:42 +1000

    Functions are normal callable go functions rather than cloud functions

--- heating/HeaterOff/HeaterOff.go
+++ heating/HeaterOff/HeaterOff.go
@@ -1,11 +1,10 @@
-package HeaterOff
+package main
 
 import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/jake-mok-nelson/kasa-go"
@@ -13,7 +12,7 @@ import (
 
 // Turns the heater off
 
-func HeaterOff(w http.ResponseWriter, r *http.Request) {
+func main() {
 
 	type Credentials struct {
 		Username string `json:"username"`
@@ -24,54 +23,32 @@ func HeaterOff(w http.ResponseWriter, r *http.Request) {
 	jsonFile, err := os.Open("creds.json")
 	// if we os.Open returns an error then handle it
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
 	defer jsonFile.Close()
 	byteValue, err := ioutil.ReadAll(jsonFile)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
 	var creds Credentials
 
 	err = json.Unmarshal(byteValue, &creds)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
-
 	api, err := kasa.Connect(creds.Username, creds.Password)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
 
 	heater, err := api.GetHS100("Heater")
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
-
 	println("I was told to turn the heater off, so I guess I'll do that")
 	err = heater.TurnOff()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
 	fmt.Print("Heater turned off successfully!")
-	// all good. write our message.
-	w.WriteHeader(http.StatusOK)
 }
--- heating/HeaterOn/HeaterOn.go
+++ heating/HeaterOn/HeaterOn.go
@@ -1,11 +1,10 @@
-package HeaterOn
+package main
 
 import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 
 	owm "github.com/briandowns/openweathermap"
@@ -18,7 +17,7 @@ import (
 var tempThreshold float64 = 15.0 // If the weather is equal to or below this, we will turn on the heater
 var city string = "Melbourne"    // Where we are
 
-func HeaterOn(w http.ResponseWriter, r *http.Request) {
+func main() {
 
 	type Credentials struct {
 		Username   string `json:"username"`
@@ -28,75 +27,48 @@ func HeaterOn(w http.ResponseWriter, r *http.Request) {
 
 	jsonFile, err := os.Open("creds.json")
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
 	defer jsonFile.Close()
 	byteValue, err := ioutil.ReadAll(jsonFile)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
 	var creds Credentials
 
 	err = json.Unmarshal(byteValue, &creds)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
 
 	api, err := kasa.Connect(creds.Username, creds.Password)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
 
 	heater, err := api.GetHS100("Heater")
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
 
 	wh, err := owm.NewCurrent("C", "en", creds.WeatherKey) // Celsius, english, weatherapi key
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
 	err = wh.CurrentByName(city)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
 		log.Println(err.Error())
-		return
 	}
 	currentTemp := wh.Main.Temp //Let's make this shit more readable
 	if wh.Main.Temp <= tempThreshold {
 		fmt.Printf("\nIt's pretty chilly in %s at %f, turning on the heater.\n", city, currentTemp)
+
 		err = heater.TurnOn()
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
 			log.Println(err.Error())
-			return
 		}
 		fmt.Print("Heater turned on successfully!")
 	} else {
 		println("Heat is above threshold so I won't turn the heater on.")
 	}
-
-	// all good. write our message.
-	w.WriteHeader(http.StatusOK)
-
 }
